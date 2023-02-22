f1e62e4d1d9d14c5e61dd11e455319ebd66cf20e
Kickblip <wyatt.zilker@gmail.com>
2023-02-22 11:31:13 -0600

    init

---
+++ src/index.js
@@ -0,0 +1,84 @@
+import * as THREE from 'three';
+
+import { AsciiEffect } from 'three/addons/effects/AsciiEffect.js';
+import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
+
+let camera, controls, scene, renderer, effect;
+
+let sphere;
+
+init();
+animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.y = 150;
+    camera.position.z = 500;
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0, 0, 0);
+
+    const pointLight1 = new THREE.PointLight(0xffffff);
+    pointLight1.position.set(500, 500, 500);
+    scene.add(pointLight1);
+
+    const pointLight2 = new THREE.PointLight(0xffffff, 0.25);
+    pointLight2.position.set(- 500, - 500, - 500);
+    scene.add(pointLight2);
+
+    sphere = new THREE.Mesh(new THREE.SphereGeometry(200, 20, 10), new THREE.MeshPhongMaterial({ flatShading: true }));
+    scene.add(sphere);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    effect = new AsciiEffect(renderer, ' .:-+*=%@#', { invert: true });
+    effect.setSize(window.innerWidth, window.innerHeight);
+    effect.domElement.style.color = 'white';
+    effect.domElement.style.backgroundColor = 'black';
+
+    // Special case: append effect.domElement, instead of renderer.domElement.
+    // AsciiEffect creates a custom domElement (a div container) where the ASCII elements are placed.
+
+    document.body.appendChild(effect.domElement);
+
+    controls = new TrackballControls(camera, effect.domElement);
+
+    //
+
+    window.addEventListener('resize', onWindowResize);
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    effect.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+//
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    render();
+
+}
+
+function render() {
+
+    sphere.position.y += 0.005;
+    sphere.rotation.x += 0.005;
+    sphere.rotation.z += 0.005;
+
+    controls.update();
+
+    effect.render(scene, camera);
+
+}
